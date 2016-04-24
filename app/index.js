f3e84a2ae181b20398594c4eabb2f0c5bf9a714d
Loc M Nguyen <lokywin@gmail.com>
2016-04-23 20:07:14 -0700

    Testing with react-vimeo package.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Timer from '../components/Timer';
+import Vimeo from 'react-vimeo';
+
+let videoId = '10517961';
 
 render(
   <Timer />,
   document.getElementById('timer')
+);
+
+render(
+  <Vimeo videoId={ videoId } />,
+  document.getElementById('video')
 );
